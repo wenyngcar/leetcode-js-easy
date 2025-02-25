f296193a9fe8d45ed43f02753a23cbe4a60464ca
wenyngcar <owenyoungcarino@gmail.com>
2025-02-25 13:19:44 +0800

    feat: initialize

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /**
- * @return {null|boolean|number|string|Array|Object}
+ * @param {number} n
+ * @return {Function} counter
  */
-Array.prototype.last = function() {
-  return this.length > 0 ? this[this.length - 1] : -1
+var createCounter = function(n) {
+  var result = n
+  var add = -1
+  return function() {
+    add += 1
+    return result + add
+  };
 };
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
