0b062df607a9daef8c00ca6e89fd8cbc90a42a35
wenyngcar <owenyoungcarino@gmail.com>
2025-03-17 22:37:06 +0800

    feat: initialize

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 /**
- * @param {Function[]} functions
- * @return {Function}
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
  */
-var compose = function(functions) {
-  return function(x) {
-    for (let i = functions.length - 1; i >= 0; i--) {
-      x = functions[i](x);
-    }
-    return x;
-  };
-};
+var filter = function(arr, fn) {
 
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
+};
