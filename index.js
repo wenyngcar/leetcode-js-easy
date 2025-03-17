d689ddb2980c913ecc4abd2313067c6fa68729a7
wenyngcar <owenyoungcarino@gmail.com>
2025-03-17 23:10:41 +0800

    feat: code accepted

--- index.js
+++ index.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-
-};
+  const filteredArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i])
+    }
+  }
+  return filteredArr
+}
