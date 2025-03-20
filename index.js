f2661ef06879cfc686d1967b09cdaa831787dcd4
wenyngcar <owenyoungcarino@gmail.com>
2025-03-20 20:54:01 +0800

    feat: code accepted

--- index.js
+++ index.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+  const result = []
+  for (let i = 0; i < arr.length; i++) {
+    let temp = fn(arr[i], i)
+    result.push(temp)
+  }
 
+  return result
 };
