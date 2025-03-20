fc8cc7f6bfd21ac0f272f1c60777629019d2e531
wenyngcar <owenyoungcarino@gmail.com>
2025-03-20 20:52:23 +0800

    feat: initialize

--- index.js
+++ index.js
@@ -3,12 +3,6 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function(arr, fn) {
-  const filteredArr = []
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i], i)) {
-      filteredArr.push(arr[i])
-    }
-  }
-  return filteredArr
-}
+var map = function(arr, fn) {
+
+};
